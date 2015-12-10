03a9c1c800b41dc720e93883e0744ea2d8bd67aa
Carl Vitullo <carl.vitullo@gmail.com>
2015-12-10 17:56:41 -0500

    Add validated route handlers

---
+++ src/validatedRoute.js
@@ -0,0 +1,87 @@
+import _ from 'lodash'
+import Validator from 'validator'
+import Restify from 'restify'
+
+/**
+ * (string, string) -> typedValue
+ */
+function convert(type, value) {
+  switch (type) {
+  case 'integer':
+    return Validator.toInt(value);
+  case 'float':
+    return Validator.toFloat(value);
+  case 'string':
+    return Validator.toString(value);
+  case 'date':
+    return Validator.toDate(value);
+  case 'json':
+    return value;
+  case 'uuid':
+    return value;
+  }
+}
+
+/**
+ * (string, string) -> bool
+ */
+function isValid(type, value) {
+  switch (type) {
+  case 'integer':
+    return Validator.isInt(value);
+  case 'float':
+    return Validator.isFloat(value);
+  case 'string':
+    return Validator.isString(value);
+  case 'date':
+    return Validator.isDate(value);
+  case 'json':
+    return Validator.isJSON(value);
+  case 'uuid':
+    return Validator.isUUID(value);
+  }
+}
+
+/**
+ * (function, array) -> function
+ * validatedRoute takes a function to handle the route and an array of objects
+ * with the shape {field, type, required}.
+ */
+export default function validatedRoute(handler, typeDefinitions) {
+  let types = [
+    'integer'
+    ,'float'
+    ,'string'
+    ,'date'
+    ,'json'
+    ,'uuid'
+  ];
+
+  return function route(req, res, next) {
+    let validationErrors = [];
+    let fieldType = {};
+
+    for (fieldType of typeDefinitions) {
+      let { field, type, required } = fieldType;
+      let value = req.body[field];
+
+      if (required && Validator.isNull(value)) {
+        validationErrors.push(`Missing required field: '${field}'`);
+      }
+      else if (!isValid(type, value)) {
+        validationErrors.push(`Invalid value: ${field}: '${value}' should be of type '${type}'`);
+      }
+      else {
+        req.body[field] = convert(type, value);
+      }
+    }
+
+    if (validationErrors.length > 0) {
+      res.send(new Restify.errors.BadRequestError(validationErrors.join(', ')));
+      return next();
+    }
+    else {
+      return handler(req, res, next);
+    }
+  }
+}
