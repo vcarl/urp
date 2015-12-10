e7985f8150895277531d78c90714783f40c7bbe4
Carl Vitullo <carl.vitullo@gmail.com>
2015-12-10 17:56:50 -0500

    Validate buy route

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import Restify from 'restify'
 import uuid    from 'uuid'
 import { buyItem, sellItem } from './inventory/index'
+import validatedRoute from './validatedRoute'
 
-var server = Restify.createServer();
-server.use(Restify.bodyParser())
-server.post('/inventory/:id/buy', buyItem);
+let server = Restify.createServer();
+server.use(Restify.bodyParser());
+
+let buyTypes = [
+  {field: 'quantity', type: 'integer', required: true}
+  ,{field: 'price', type: 'float', required: true}
+]
+
+server.post('/inventory/:id/buy', validatedRoute(buyItem, buyTypes));
 server.post('/inventory/:id/sell', sellItem);
 
 server.listen(8080, function() {
